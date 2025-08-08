81f2ca704de939a751f48a48216015a573b943ed
Artemis Li <auroraziling@gmail.com>
2025-08-08 09:38:47 +0800

    docs: HorizontalCenterImg component

---
+++ docs/.vitepress/components/Common/HorizontalCenterImg.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-center">
+    <img :src="src" :alt="alt" :width="width" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: String,
+    default: '500px',
+  },
+});
+</script>
