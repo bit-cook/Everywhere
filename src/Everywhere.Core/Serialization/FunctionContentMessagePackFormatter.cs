06fdcb493252ad8695e66ab3e82be2b1c777452d
Dear.Va <dear.va.love@gmail.com>
2026-01-25 21:22:17 +0800

    fix: msgpack JsonElement serialization

--- src/Everywhere.Core/Serialization/FunctionContentMessagePackFormatter.cs
+++ src/Everywhere.Core/Serialization/FunctionContentMessagePackFormatter.cs
@@ -1,4 +1,5 @@
-﻿using MessagePack;
+﻿using System.Text.Json;
+using MessagePack;
 using MessagePack.Formatters;
 
 namespace Everywhere.Serialization;
@@ -37,7 +38,7 @@ public abstract class FunctionContentMessagePackFormatter<T> : IMessagePackForma
 
     protected abstract T LegacyDeserializeCore(ref MessagePackReader reader, MessagePackSerializerOptions options);
 
-    protected void SerializeDictionary(
+    protected static void SerializeDictionary(
         ref MessagePackWriter writer,
         IReadOnlyDictionary<string, object?>? value,
         MessagePackSerializerOptions options)
@@ -55,12 +56,20 @@ public abstract class FunctionContentMessagePackFormatter<T> : IMessagePackForma
             }
             else
             {
-                MessagePackSerializer.Serialize(val.GetType(), ref writer, val, options);
+                var type = val.GetType();
+                if (!TypeHelper.TypeCodes.TryGetValue(type, out var typeCode))
+                {
+                    throw new MessagePackSerializationException($"Unsupported dictionary value type: {type.FullName}");
+                }
+
+                writer.WriteArrayHeader(2);
+                writer.Write(typeCode);
+                MessagePackSerializer.Serialize(type, ref writer, val, options);
             }
         }
     }
 
-    protected Dictionary<string, object?>? DeserializeDictionary(
+    protected static Dictionary<string, object?>? DeserializeDictionary(
         ref MessagePackReader reader,
         MessagePackSerializerOptions options)
     {
@@ -71,10 +80,35 @@ public abstract class FunctionContentMessagePackFormatter<T> : IMessagePackForma
         for (var i = 0; i < count; i++)
         {
             var key = reader.ReadString() ?? throw new MessagePackSerializationException("Dictionary key cannot be null.");
-            var val = MessagePackSerializer.Deserialize<object>(ref reader, options);
+
+            reader.ReadArrayHeader();
+            var typeCode = reader.ReadInt32();
+            if (typeCode == 0 || !TypeHelper.CodeTypes.TryGetValue(typeCode, out var targetType))
+            {
+                throw new MessagePackSerializationException($"Unsupported dictionary value type code: {typeCode}");
+            }
+
+            var val = MessagePackSerializer.Deserialize(targetType, ref reader, options);
             dict[key] = val;
         }
 
         return dict;
     }
+}
+
+file static class TypeHelper
+{
+    public static readonly IReadOnlyDictionary<Type, int> TypeCodes = new Dictionary<Type, int>(8)
+    {
+        { typeof(string), 1 },
+        { typeof(int), 2 },
+        { typeof(long), 3 },
+        { typeof(float), 4 },
+        { typeof(double), 5 },
+        { typeof(bool), 6 },
+        { typeof(byte[]), 7 },
+        { typeof(JsonElement), 8 },
+    };
+
+    public static readonly IReadOnlyDictionary<int, Type> CodeTypes = TypeCodes.ToDictionary(kv => kv.Value, kv => kv.Key);
 }
