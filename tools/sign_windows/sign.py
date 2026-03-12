b9c25bd78ae1671da17ea328f3fa90f82e9fb608
DearVa <dear.va.love@gmail.com>
2026-03-13 02:30:10 +0800

    build(Windows): add codesign for exe installer

--- tools/sign_windows/sign.py
+++ tools/sign_windows/sign.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b575acb4baa0b85bdc223a1ce79e03abd5caea3ae96e86e7978aeffd055fb470
-size 2736
+oid sha256:16d100b396d8092f5d564cbe7a2262f9e7abeebce547e4025a62bb45fc045393
+size 5394
