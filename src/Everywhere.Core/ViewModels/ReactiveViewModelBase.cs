12d998eb909a18ea1eb1156f10c7ab4b31fa7620
Dear.Va <dear.va.love@gmail.com>
2026-04-16 22:04:47 +0800

    fix(macOS): StorageProvider is not available

--- src/Everywhere.Core/ViewModels/ReactiveViewModelBase.cs
+++ src/Everywhere.Core/ViewModels/ReactiveViewModelBase.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Avalonia.Controls;
 using Avalonia.Input.Platform;
-using Avalonia.Platform;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Everywhere.Common;
@@ -29,17 +28,14 @@ public abstract class ReactiveViewModelBase : ObservableValidator
     }
 
     protected IClipboard Clipboard =>
-        _topLevelImpl?.TryGetFeature<IClipboard>() ?? throw new InvalidOperationException("Clipboard is not available.");
+        _topLevel?.Clipboard ?? throw new InvalidOperationException("Clipboard is not available.");
 
     protected IStorageProvider StorageProvider =>
-        _topLevelImpl?.TryGetFeature<IStorageProvider>() ?? throw new InvalidOperationException("StorageProvider is not available.");
+        _topLevel?.StorageProvider?? throw new InvalidOperationException("StorageProvider is not available.");
 
     protected static ILauncher Launcher => BetterBclLauncher.Shared;
 
-    protected IScreenImpl ScreenImpl =>
-        _topLevelImpl?.TryGetFeature<IScreenImpl>() ?? throw new InvalidOperationException("ScreenImpl is not available.");
-
-    private ITopLevelImpl? _topLevelImpl;
+    private TopLevel? _topLevel;
 
     protected AnonymousExceptionHandler DialogExceptionHandler => new((exception, message, source, lineNumber) =>
         DialogManager.CreateDialog(exception.GetFriendlyMessage().ToString() ?? "Unknown error", $"[{source}:{lineNumber}] {message ?? "Error"}"));
@@ -80,9 +76,8 @@ public abstract class ReactiveViewModelBase : ObservableValidator
         {
             try
             {
-                var topLevel = TopLevel.GetTopLevel(target);
-                _topLevelImpl = topLevel?.PlatformImpl;
-                if (topLevel is IReactiveHost reactiveHost)
+                _topLevel = TopLevel.GetTopLevel(target);
+                if (_topLevel is IReactiveHost reactiveHost)
                 {
                     DialogManager = reactiveHost.DialogHost.Manager;
                     ToastManager = reactiveHost.ToastHost.Manager;
@@ -98,7 +93,7 @@ public abstract class ReactiveViewModelBase : ObservableValidator
 
         target.Unloaded += async (_, _) =>
         {
-            _topLevelImpl = null;
+            _topLevel = null;
             cancellationTokenSource.Cancel();
 
             try
